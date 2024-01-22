c3f22154bd719f84315f84749d6acc7b49828b9c
conkr11 <conkr11@partner.waters.com>
2024-01-22 12:22:43 +0530

    Modified the feature file and implemented step definition based on new FF

--- LoginDemo/LoginSF/Features/Signin.feature
+++ LoginDemo/LoginSF/Features/Signin.feature
@@ -2,16 +2,13 @@ Feature: Sign In
 
 @mytag
 Scenario: Sign in to the browser
-
-	Given Load the browser
-	When login page opens
-	Then enter username password confirm password
-
-    When Attempt log in using incorrect '<username>','<password>' and '<cpassword>'
-    Then the user receives access invalid
-
-
-    When Attempt log in using correct '<username>','<password>' and '<cpassword>'
-    Then the user receives log in success message
-
     
+    Given Chrome browser is opened
+    When I open Login Page 
+        And I enter the username 'user1', password '123456' and confirm_password '123456'
+    When password '12345' and confirm_password '123456' are not same
+    Then user receives Error message
+
+    When password '123456' and confirm_password '123456' are same
+    Then user receives Success message
+   
--- LoginDemo/LoginSF/StepDefinitions/SignInSteps.cs
+++ LoginDemo/LoginSF/StepDefinitions/SignInSteps.cs
@@ -1,5 +1,7 @@
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using TechTalk.SpecFlow;
 using TechTalk.SpecFlow.Assist;
 
@@ -11,57 +13,70 @@ namespace LoginSF.StepDefinitions
         private IWebDriver driver;
         private readonly string baseurl = "https://localhost:44318/Registration.html";
 
-        [Given(@"Load the browser")]     
-        [When(@"login page opens")]
-        public void WhenLoginPageOpens()
+        [Given(@"Chrome browser is opened")]
+        public void GivenChromeBrowserIsOpened()
         {
             driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();  
+            driver.Manage().Window.Maximize();
         }
 
-        [Then(@"enter username password confirm password")]
-        public void ThenEnterUsernamePasswordConfirmPassword()
+        [When(@"I open Login Page")]
+        public void WhenIOpenLoginPage()
         {
             driver.Navigate().GoToUrl(baseurl);
-            driver.FindElement(By.Id("username")).SendKeys("user1");
-            driver.FindElement(By.Id("password")).SendKeys("123456");
-            driver.FindElement(By.Id("cpassword")).SendKeys("12345");
-            Thread.Sleep(2000);
         }
 
-
-        [When(@"Attempt log in using incorrect '([^']*)','([^']*)' and '([^']*)'")]
-        public void WhenAttemptLogInUsingIncorrectAnd(string username, string password, string cpassword)
+        [When(@"I enter the username '([^']*)', password '([^']*)' and confirm_password '([^']*)'")]
+        public void WhenIEnterTheUsernamePasswordAndConfirm_Password(string username, string password, string cpassword)
         {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+
+            IWebElement user = driver.FindElement(By.Id("username"));
+            user.SendKeys(username);
+            IWebElement pwd = driver.FindElement(By.Id("password"));
+            pwd.SendKeys(password);
+            IWebElement cpwd = driver.FindElement(By.Id("cpassword"));
+            cpwd.SendKeys(cpassword);
+        }
+
+        [When(@"password '([^']*)' and confirm_password '([^']*)' are not same")]
+        public void WhenPasswordAndConfirm_PasswordAreNotSame(string password, string cpassword)
+        {
+            Assert.IsNotNull(password);
+            Assert.IsNotNull(cpassword);
+            ScenarioContext.Current["incorrect_pwd"] = password;
+            ScenarioContext.Current["incorrect_cpwd"] = cpassword;
             driver.FindElement(By.Id("btn-sub")).Click();
-            Thread.Sleep(3000);
+        }
+
+        [Then(@"user receives Error message")]
+        public void ThenUserReceivesErrorMessage()
+        {
+            string password = ScenarioContext.Current["incorrect_pwd"].ToString();
+            string cpassword = ScenarioContext.Current["incorrect_cpwd"].ToString();
+            Assert.AreNotEqual(password, cpassword);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
             driver.Navigate().GoToUrl("https://localhost:44318/Error.html");
-           
         }
 
-        [Then(@"the user receives access invalid")]
-        public void ThenTheUserReceivesAccessInvalid()
+        [When(@"password '([^']*)' and confirm_password '([^']*)' are same")]
+        public void WhenPasswordAndConfirm_PasswordAreSame(string password, string cpassword)
         {
-            Console.WriteLine("User Registered Failed");
-            Thread.Sleep(5000);
-        }
-
-        [When(@"Attempt log in using correct '([^']*)','([^']*)' and '([^']*)'")]
-        public void WhenAttemptLogInUsingCorrectAnd(string username, string password, string cpassword)
-        {
-            driver.Navigate().GoToUrl(baseurl);
-            driver.FindElement(By.Id("username")).SendKeys("user1");
-            driver.FindElement(By.Id("password")).SendKeys("123456");
-            driver.FindElement(By.Id("cpassword")).SendKeys("123456");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+            driver.Navigate().Back();
+            ScenarioContext.Current["correct_pwd"] = password;
+            ScenarioContext.Current["correct_cpwd"] = cpassword;
             driver.FindElement(By.Id("btn-sub")).Click();
-            Thread.Sleep(2000);
-            driver.Navigate().GoToUrl("https://localhost:44318/Success.html");
         }
 
-        [Then(@"the user receives log in success message")]
-        public void ThenTheUserReceivesLogInSuccessMessage()
+        [Then(@"user receives Success message")]
+        public void ThenUserReceivesSuccessMessage()
         {
-            Console.WriteLine("User Registered Sucessfully");
+            string password = ScenarioContext.Current["correct_pwd"].ToString();
+            string cpassword = ScenarioContext.Current["correct_cpwd"].ToString();
+            Assert.AreEqual(password, cpassword);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+            driver.Navigate().GoToUrl("https://localhost:44318/Success.html");
         }
 
     }
