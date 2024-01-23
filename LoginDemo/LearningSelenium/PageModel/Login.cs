8b997753bef1cfbb9cff61e7c5991605f2e3513c
conkr11 <conkr11@partner.waters.com>
2024-01-23 19:00:22 +0530

    page object model implemented

---
+++ LoginDemo/LearningSelenium/PageModel/Login.cs
@@ -0,0 +1,84 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace LearningSelenium.PageModel
+{
+    public class Login
+    {
+        WebDriver driver;
+        protected By UserName = By.XPath("//input[contains(@name,'username')]");
+        protected By Password = By.XPath("//input[contains(@name,'password')]");
+        protected By LogButton = By.XPath("//button[contains(@type,'submit') ]");
+        public By LogError = By.XPath("//p[contains(@class,'oxd-text') and contains(@class,'oxd-text--p') and contains(@class,'oxd-alert-content-text') ]");
+        public By LogSuccess = By.XPath("//h6[contains(@class,'oxd-text')and contains(@class,'oxd-text--h6') and contains(@class,'oxd-topbar-header-breadcrumb-module')]");
+
+        protected By SUserName = By.XPath("//input[contains(@id,'user-name')]");
+        protected By SPassword = By.XPath("//input[contains(@id,'password')]");
+        protected By SLogButton = By.XPath("//input[contains(@id,'login-button') ]");
+        public By SLogError = By.XPath("//div[contains(@class,'error-message-container') and contains(@class,'error')]/h3");
+        public By SLogSuccess = By.XPath("//div[contains(@class,'header_secondary_container')]/span[contains(@class,'title')]");
+
+
+        public Login(WebDriver driver)
+        {
+            this.driver = driver;
+        }
+        public void enterUserName(string userName)
+        {
+            driver.FindElement(UserName).SendKeys(userName);
+        }
+        public void enterPassword(string password)
+        {
+            driver.FindElement(Password).SendKeys(password);
+        }
+        public void LoginClick()
+        {
+            driver.FindElement(LogButton).Click();
+        }
+        public void enterSwagUserName(string userName)
+        {
+            driver.FindElement(SUserName).SendKeys(userName);
+        }
+        public void enterSwagPassword(string password)
+        {
+            driver.FindElement(SPassword).SendKeys(password);
+        }
+        public void SwagLoginClick()
+        {
+            driver.FindElement(SLogButton).Click();
+        }
+        public string getErrorMessage()
+        {
+            string strErrorMsg = null;
+            var errorElement = driver.FindElement(LogError);
+            strErrorMsg = (errorElement.Text.Length>0) ? errorElement.Text : null;
+            return strErrorMsg;
+        }
+        public string getSuccessMessage()
+        {
+            string strSuccessMsg = null;
+            var sucessElement = driver.FindElement(LogSuccess);
+            strSuccessMsg = (sucessElement.Text.Length > 0) ? sucessElement.Text : null;
+            return strSuccessMsg;
+        }
+        public string getSwagErrorMessage()
+        {
+            string strErrorMsg = null;
+            var errorElement = driver.FindElement(SLogError);
+            strErrorMsg = (errorElement.Text.Length > 0) ? errorElement.Text : null;
+            return strErrorMsg;
+        }
+        public string getSwagSuccessMessage()
+        {
+            string strSuccessMsg = null;
+            var sucessElement = driver.FindElement(SLogSuccess);
+            strSuccessMsg = (sucessElement.Text.Length > 0) ? sucessElement.Text : null;
+            return strSuccessMsg;
+        }
+    }
+}
---
+++ LoginDemo/LearningSelenium/Shopping.cs
@@ -0,0 +1,139 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LearningSelenium.PageModel;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+
+namespace LearningSelenium
+{
+    internal class Shopping
+    {
+        WebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+        }
+
+        [Test]
+        public void TestLoginFail()
+        {
+            Login lg = new Login(driver);
+            driver.Navigate().GoToUrl("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            lg.enterUserName("admin");
+            lg.enterPassword("admin1234");
+            lg.LoginClick();
+
+            WebDriverWait wait= new WebDriverWait(driver,TimeSpan.FromSeconds(20));
+            wait.Until(d => lg.getErrorMessage());
+            Assert.AreEqual("Invalid credentials", lg.getErrorMessage(),"username or password is incorrect");
+
+        }
+
+        [Test]
+        public void TestLoginSuccess()
+        {
+            Login lg = new Login(driver);
+            driver.Navigate().GoToUrl("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            lg.enterUserName("Admin");
+            lg.enterPassword("admin123");
+            lg.LoginClick();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            wait.Until(d => lg.getSuccessMessage());
+            Assert.AreEqual("Dashboard", lg.getSuccessMessage(), "Dashboard not loading...");
+
+        }
+
+        [Test]
+        public void TestLogFail()
+        {
+            Login lg = new Login(driver);
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            lg.enterSwagUserName("user");
+            lg.enterSwagPassword("secert_sauce");
+            lg.SwagLoginClick();
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            wait.Until(d => lg.getSwagErrorMessage());
+            Assert.AreEqual("Epic sadface: Username and password do not match any user in this service", lg.getSwagErrorMessage(), "username or password is incorrect");
+
+        }
+        
+        [Test]
+        public void TestLogSuccess()
+        {
+            Login lg = new Login(driver);
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            lg.enterSwagUserName("standard_user");
+            lg.enterSwagPassword("secret_sauce");
+            lg.SwagLoginClick();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            wait.Until(d => lg.getSwagSuccessMessage());
+            Assert.AreEqual("Products", lg.getSwagSuccessMessage(), "loged user name is mismatch");
+
+        }
+        [Test]
+        public void TestPurchase()
+        {
+            Login lg = new Login(driver);
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            lg.enterSwagUserName("visual_user");
+            lg.enterSwagPassword("secret_sauce");
+            lg.SwagLoginClick(); 
+            Product p = new Product(driver);
+            driver.Manage().Timeouts().ImplicitWait=TimeSpan.FromSeconds(20);
+            p.getItem1();
+            p.LinkItem1();            
+            driver.FindElement(p.BackMenu).Click();
+            p.getItem2();
+            p.LinkItem2();            
+            driver.FindElement(p.BackMenu).Click();
+            Assert.Greater(p.getItemCount(), 0, "Shopping card is empty");
+        }
+        [Test]
+        public void TestShopping()
+        {
+            Login lg = new Login(driver);
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            lg.enterSwagUserName("visual_user");
+            lg.enterSwagPassword("secret_sauce");
+            lg.SwagLoginClick(); 
+            Product p = new Product(driver);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            p.getItem1();
+            p.LinkItem1();
+            driver.FindElement(p.BackMenu).Click();
+            p.getItem2();
+            p.LinkItem2();
+            driver.FindElement(p.BackMenu).Click();
+            Purchase s=new Purchase(driver);
+            driver.Manage().Timeouts().ImplicitWait=TimeSpan.FromSeconds(20);
+            driver.FindElement(s.Cart).Click();
+            driver.FindElement(s.Checkout).Click();
+            s.enterFirstName("Mark");
+            s.enterLastName("David");
+            s.enterPostal("707872");
+            driver.FindElement(s.Continue).Click();
+            Assert.Greater(s.getPrice(), 0.00, "Shopping card is empty");
+        }
+        [TearDown]
+        public void TearDown()
+        {
+           driver.Close();
+        }
+    }
+    }
